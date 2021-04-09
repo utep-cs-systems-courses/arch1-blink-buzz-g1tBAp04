2a3db7d1195391d2498a1f7db5de69aea2522a58
g1tBAp04 <djbaptiste@miners.utep.edu>
2021-04-09 12:20:31 -0600

    buzzer roadblock

--- project/buttontest/buttonMain.c
+++ project/buttontest/buttonMain.c
@@ -3,11 +3,15 @@
 #include "led.h"
 #include "switches.h"
 #include "buttonstate.h"
+#include "buzzer.h"
 
 void main(void) 
 {  
   configureClocks();
+  enableWDTInterrupts();
   switch_init();
+  buzzer_init();
+  buzzer_set_period(0);
   led_init();
 
   or_sr(0x18);  // CPU off, GIE on
